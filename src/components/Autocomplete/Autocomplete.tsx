4a3afe50b9f29588a8e818f0d56a1b027bfed521
lidiia-tsymborovych <lidacimbirovic@gmail.com>
2025-06-01 14:54:52 +0300

    divide Autocomplete into smaller components

--- src/components/Autocomplete/Autocomplete.tsx
+++ src/components/Autocomplete/Autocomplete.tsx
@@ -1,9 +1,16 @@
-import cn from 'classnames';
-import React, { useEffect, useMemo, useRef, useState } from 'react';
+import React, {
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react';
 import debounce from 'lodash.debounce';
 
 import { Person } from '../../types/Person';
 import { getFilteredPeople } from '../../utils/getFilteredPeople';
+import { SuggestionList } from '../SuggestionList/';
+import { NoSuggestionsMessage } from '../NoSuggestionsMessage/';
 
 type Props = {
   people: Person[];
@@ -25,8 +32,10 @@ export const Autocomplete: React.FC<Props> = ({
   useEffect(() => {
     if (selectedPerson) {
       setInputValue(selectedPerson.name);
+      setQuery(selectedPerson.name.toLowerCase());
     } else {
       setInputValue('');
+      setQuery('');
     }
   }, [selectedPerson]);
 
@@ -38,22 +47,22 @@ export const Autocomplete: React.FC<Props> = ({
 
   const wrapperRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      if (
-        wrapperRef.current &&
-        !wrapperRef.current.contains(event.target as Node)
-      ) {
-        setIsFocused(false);
-      }
-    };
+  const handleClickOutside = useCallback((event: MouseEvent) => {
+    if (
+      wrapperRef.current &&
+      !wrapperRef.current.contains(event.target as Node)
+    ) {
+      setIsFocused(false);
+    }
+  }, []);
 
+  useEffect(() => {
     document.addEventListener('mousedown', handleClickOutside);
 
     return () => {
       document.removeEventListener('mousedown', handleClickOutside);
     };
-  }, []);
+  }, [handleClickOutside]);
 
   useEffect(() => {
     return () => {
@@ -61,96 +70,74 @@ export const Autocomplete: React.FC<Props> = ({
     };
   }, [debouncedSetQuery]);
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const value = event.target.value.trimStart();
-
-    setInputValue(value);
-    debouncedSetQuery(value);
-    setIsFocused(true);
-
-    if (selectedPerson && value !== selectedPerson.name) {
-      onSelected(null);
-    }
-  };
-
-  const handleInputFocus = () => {
-    setIsFocused(true);
-    debouncedSetQuery('');
-  };
-
-  const handleClickSelectPerson = (selected: Person) => {
-    onSelected(selected);
-    setIsFocused(false);
-    setInputValue(selected.name);
+  const clearSelection = useCallback(() => {
+    onSelected(null);
     setQuery('');
-  };
+  }, [onSelected]);
+
+  const handleInputChange = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      const value = event.target.value.trim().toLowerCase();
+
+      setInputValue(value);
+      setIsFocused(true);
+
+      if (selectedPerson && value === '') {
+        clearSelection();
+      }
+
+      if (value === '') {
+        debouncedSetQuery.cancel();
+        setQuery('');
+      } else {
+        debouncedSetQuery(value);
+      }
+    },
+    [selectedPerson, clearSelection, debouncedSetQuery],
+  );
+
+  const handleClickSelectPerson = useCallback(
+    (person: Person) => {
+      onSelected(person);
+      setIsFocused(false);
+      setInputValue(person.name);
+      setQuery(person.name.toLowerCase());
+    },
+    [onSelected],
+  );
+
+  const handleFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
 
   return (
-    <>
-      <div className="dropdown is-active" ref={wrapperRef}>
-        <div className="dropdown-trigger">
-          <input
-            type="text"
-            placeholder="Enter a part of the name"
-            className="input"
-            data-cy="search-input"
-            value={inputValue}
-            onChange={handleInputChange}
-            onFocus={handleInputFocus}
-            id="autocomplete-input"
-            aria-autocomplete="list"
-            aria-controls="suggestions-list"
-            aria-expanded={isFocused}
-          />
-        </div>
-
-        {isFocused && filteredPeople.length > 0 && (
-          <div
-            className="dropdown-menu"
-            role="menu"
-            id="suggestions-list"
-            data-cy="suggestions-list"
-          >
-            <div className="dropdown-content">
-              {filteredPeople.map(person => {
-                return (
-                  <div
-                    key={person.slug}
-                    className="dropdown-item"
-                    data-cy="suggestion-item"
-                    onClick={() => handleClickSelectPerson(person)}
-                  >
-                    <p
-                      className={cn({
-                        'has-text-link': person.sex === 'm',
-                        'has-text-danger': person.sex === 'f',
-                      })}
-                    >
-                      {person.name}
-                    </p>
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-        )}
+    <div className="dropdown is-active" ref={wrapperRef}>
+      <div className="dropdown-trigger">
+        <input
+          type="text"
+          placeholder="Enter a part of the name"
+          className="input"
+          data-cy="search-input"
+          value={inputValue}
+          onChange={handleInputChange}
+          onFocus={handleFocus}
+          id="autocomplete-input"
+          aria-autocomplete="list"
+          aria-controls="suggestions-list"
+          aria-expanded={isFocused}
+        />
       </div>
 
-      {isFocused && query && filteredPeople.length === 0 && (
-        <div
-          className="
-            notification
-            is-danger
-            is-light
-            mt-3
-            is-align-self-flex-start
-          "
-          role="alert"
-          data-cy="no-suggestions-message"
-        >
-          <p className="has-text-danger">No matching suggestions</p>
-        </div>
+      {isFocused && filteredPeople.length > 0 && (
+        <SuggestionList
+          people={filteredPeople}
+          onSelectPerson={handleClickSelectPerson}
+        />
       )}
-    </>
+
+      {isFocused && query && filteredPeople.length === 0 && (
+        <NoSuggestionsMessage />
+      )}
+    </div>
   );
 };
